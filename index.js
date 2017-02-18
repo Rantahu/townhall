c4c8fe9d19a9f1eaf896cf4ee0be120661af3143
Rokney Rosemie <Rantahu@gmail.com>
2017-02-18 03:31:51 -0500

    set up basic webserver

--- index.js
+++ index.js
@@ -2,12 +2,21 @@ var express = require('express');
 
 var app = express();
 
-app.set('port', process.env.PORT || 3000);
+app.disable('x-powered-by')
 
-app.get('/', function(req,res){
-  res.send('Express Works');
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+app.set('port', process.env.PORT || 3000);
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res){
+  res.render('home');
 });
+
 
 app.listen(app.get('port'), function(){
-  console.log('Express started press Ctrl-C to terminate')
-});
+  console.log("Express started on localhost:" + app.get('port') + ' press Ctrl-C')
+})
