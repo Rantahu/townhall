a5b573ba24ae1cf8d9b662a73c12077f04d75578
juan <jj1913@nyu.edu>
2017-02-18 05:54:30 -0500

    asd

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    title: 'Express' ,
+    userID: '1234'
+  });
 });
 
 module.exports = router;
