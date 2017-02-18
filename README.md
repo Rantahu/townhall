c6ae581f888d5dc2fcd5ce82267d05f2929be70f
Rokney Rosemie <Rantahu@gmail.com>
2017-02-18 04:55:41 -0500

    quick change

--- README.md
+++ README.md
@@ -7,4 +7,6 @@ townhall
 ```
 npm express
 npm express-handlebars
+npm body-parser
+npm cookie-parser
 ```
--- index.js
+++ index.js
@@ -9,6 +9,12 @@ var handlebars = require('express-handlebars').create({defaultLayout:'main'});
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
+app.use(require('body-parser').urlencoded({
+  extended: true
+}));
+
+//
+
 app.set('port', process.env.PORT || 3000);
 app.use(express.static(__dirname + '/public'));
 
